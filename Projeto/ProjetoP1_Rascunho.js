e86983ce965be7a62bf3b1173d9cee54a9302d9c
Tammyris <tammyriscarvalho@gmail.com>
2021-07-02 15:58:52 -0300

    QuestaoGabaritada

--- Projeto/ProjetoP1_Rascunho.js
+++ Projeto/ProjetoP1_Rascunho.js
@@ -1,22 +1,24 @@
+input = require("prompt-sync")();
+
 //-------------------------------------------------------------------------------------------------------
 // CADASTRO DE QUESTÕES
 listaQuestoes = [
-    {disciplina: "Matemática", professor: "Simeão", serie: "3", assunto: "Geometria Espacial", dificuldade: "1", enunciado: "Qual é o volume de um cubo de aresta 2?", alternativa: [1,8,12,5,7,14,22,50,21,100], resposta: ["F","V","F","F","F","F","F","F","F","F"]},
-    {disciplina: "Matemática", professor: "Tammyris", serie: "1", assunto: "Geometria Plana", dificuldade: "1", enunciado: "Qual a área de um quadrado de lado 2?", alternativa: [1,4,12], resposta: ["F","V","F"]},
-    {disciplina: "História", professor: "Joana", serie: "1", assunto: "História do Brasil", dificuldade: "1", enunciado: "Quem descobriu o Brasil", alternativa: ["Fausto Silva","Fernando Henrique Cardoso","Pedro Álvares Cabral", "Tammyris Carvalho", "Marcos Vinícius"], resposta: ["F","F","V","F","F"]}
+    {disciplina: "Matematica", professor: "Simeao", serie: "3", assunto: "Geometria Espacial", dificuldade: "1", enunciado: "Qual é o volume de um cubo de aresta 2?", alternativa: [1,8,12,5,7,14,22,50,21,100], resposta: ["F","V","F","F","F","F","F","F","F","F"]},
+    {disciplina: "Matematica", professor: "Tammyris", serie: "1", assunto: "Geometria Plana", dificuldade: "1", enunciado: "Qual a área de um quadrado de lado 2?", alternativa: [1,4,12], resposta: ["F","V","F"]},
+    {disciplina: "Historia", professor: "Joana", serie: "1", assunto: "Historia do Brasil", dificuldade: "1", enunciado: "Quem descobriu o Brasil", alternativa: ["Fausto Silva","Fernando Henrique Cardoso","Pedro Álvares Cabral", "Tammyris Carvalho", "Marcos Vinícius"], resposta: ["F","F","V","F","F"]}
 ]
 
 //-------------------------------------------------------------------------------------------------------
 // ELABORAÇÃO DA PROVA
 
 // Seleção da disciplina
-var nomeDisciplina = prompt("Digite o nome da disciplina da prova");
+var nomeDisciplina = input("Digite o nome da disciplina da prova: ");
 
 // // Especificação das características da questão
-var serieDisciplina = prompt("Digite a série");
-var assuntoDisciplina = prompt("Digite o assunto");
-var dificuldadeDisciplina = prompt("Digite a dificuldade da questão");
-var quantidadeAlternativas = prompt("Digite o número de opções na questão");
+var serieDisciplina = input("Digite a série: ");
+var assuntoDisciplina = input("Digite o assunto: ");
+var dificuldadeDisciplina = input("Digite a dificuldade da questão: ");
+var quantidadeAlternativas = input("Digite o número de opções na questão: ");
 
 // // Busca das questões elegíveis + Sorteio de uma para ser utilizada 
 var questoesElegiveis = [];
@@ -29,7 +31,11 @@ for (i=0; i<=2; i++) {
         j++;
     }
 }
+
 var pos = Math.floor(Math.random() * questoesElegiveis.length);
+questaoSorteada = {
+}
+console.log(questaoSorteada);
 var questaoSorteada = questoesElegiveis[pos];
 
 // Sorteio das alternativas para a questão sorteada
@@ -45,10 +51,17 @@ for (i=0; i<quantidadeAlternativas; i++){
 
 questaoProva = {
     enunciado : "",
-    alternativa : []
+    alternativa : [],
+    gabarito : []
 };
+
 questaoProva.enunciado = questaoSorteada.enunciado;
 for (k=0; k<listaPosicoes.length; k++){
     questaoProva.alternativa[k] = questaoSorteada.alternativa[listaPosicoes[k]];
+    questaoProva.gabarito[k] = questaoSorteada.resposta[listaPosicoes[k]]
 }
 console.log(questaoProva);
+
+
+
+
